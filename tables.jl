cfc21ec36ab28d799359e7821e14f04b99e6c766
Ankur <ankurindiaofficial@gmail.com>
2023-01-06 10:45:23 +0530

    changes #002

--- tables.jl
+++ tables.jl
@@ -1,24 +1,35 @@
 using DataFrames,DelimitedFiles
-# table = DataFrame(one=1:10,two=(1:10)*2,three=(1:10)*3,four=(1:10)*4,five=(1:10)*5,six=(1:10)*6,seven=(1:10)*7,eight=(1:10)*8,nine=(1:10)*9,ten=(1:10)*10,eleven=(1:10)*11)
-function tables(number)
-    if number<=20
-        if number<1
-            print("Invalid number")
+try
+    # function tables(number)
+    #     if number<=20
+    #         if number<1
+    #             print("Invalid number")
+    #         end
+    #         if number>1
+    #             for t in 1:10
+    #                 println(t*number,)
+    #             end
+    #         end
+    #     end
+    # end
+    # t = tables(20)
+    # open("tables_1_to_11.txt","w") do io
+    #     writedlm(io,[t],',')
+    # end;
+    for r in 1:20
+        for c in 1:10
+            print(c*number,",")
         end
-        if number>1
-            for t in 1:10
-                println(t*number,)
-            end
+        println()
+    end
+
+    for features in 1:10
+        for variables in 1:10
+            print(features*variables,',')
         end
+        println()
     end
-end
-t = tables(20)
-open("tables_1_to_11.txt","w") do io
-    writedlm(io,[t],',')
-end;
-for r in 1:5
-    for c in 1:r
-        print("#")
-    end
-    println()
+
+catch
+    table = DataFrame(one=1:10,two=(1:10)*2,three=(1:10)*3,four=(1:10)*4,five=(1:10)*5,six=(1:10)*6,seven=(1:10)*7,eight=(1:10)*8,nine=(1:10)*9,ten=(1:10)*10,eleven=(1:10)*11)
 end
